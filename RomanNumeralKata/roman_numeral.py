a82886985f935816d9e1257150160f67467b2077
molnar45 <molnar45@hotmail.com>
2015-11-03 22:53:24 +0100

    new test cases and some implementation

--- RomanNumeralKata/roman_numeral.py
+++ RomanNumeralKata/roman_numeral.py
@@ -2,7 +2,7 @@ class RomanNumeral():
     def arabic_to_roman(self, arabic_value):
         if arabic_value == 0:
             return ""
-        if arabic_value == 1:
-            return "I"
+        if arabic_value == 5:
+            return "V"
 
-        return "V"
+        return "I" + self.arabic_to_roman(arabic_value - 1)
--- RomanNumeralKata/roman_numeral_tests.py
+++ RomanNumeralKata/roman_numeral_tests.py
@@ -6,5 +6,17 @@ class RomanNumeralTest(unittest.TestCase):
         rn = RomanNumeral()
         self.assertEqual("", rn.arabic_to_roman(0))
 
+    def test_one(self):
+        rn = RomanNumeral()
+        self.assertEqual("I", rn.arabic_to_roman(1))
+
+    def test_five(self):
+        rn = RomanNumeral()
+        self.assertEqual("V", rn.arabic_to_roman(5))
+
+    def test_two(self):
+        rn = RomanNumeral()
+        self.assertEqual("II", rn.arabic_to_roman(2))
+
 if __name__ == "__main__":
     unittest.main()
